28c2db15c132f60626a5f845260c1dd95c0cd020
Ninjas In The Machine <mhserver@mhdevnet.net>
2015-04-03 16:56:52 -0400

    Update DB.php

--- models/DB.php
+++ models/DB.php
@@ -7,6 +7,7 @@ use yii\db\Connection;
 use yii\db\Query;
 use yii\base\Behavior;
 use nitm\helpers\Helper;
+use nitm\helpers\ArrayHelper;
 
 //DB.php class which handles database conectivity.
 class DB extends Query
@@ -147,7 +148,7 @@ class DB extends Query
 	 */
 	public static function getDefaultDbHost()
 	{
-		preg_match("/(host=)(.+)([$;])/", \Yii::$app->db->dsn, $matches);
+		preg_match("/(host=)(.+)([$;])/", ArrayHelper::getValue(\Yii::$app, 'db.dsn', ''), $matches);
 		return $matches[2];
 	}
 	
@@ -158,7 +159,7 @@ class DB extends Query
 	 */
 	public static function getDefaultDbName()
 	{
-		preg_match("/(dbname=)(.+)($|;)/", \Yii::$app->db->dsn, $matches);
+		preg_match("/(dbname=)(.+)($|;)/", ArrayHelper::getValue(\Yii::$app, 'db.dsn', ''), $matches);
 		return $matches[2];
 	}
 	
@@ -169,7 +170,7 @@ class DB extends Query
 	 */
 	public static function getDbName()
 	{
-		preg_match("/(dbname=)(.+)($|;)/", \Yii::$app->db->dsn, $matches);
+		preg_match("/(dbname=)(.+)($|;)/", ArrayHelper::getValue(\Yii::$app, 'db.dsn', ''), $matches);
 		return $matches[2];
 	}
 	
